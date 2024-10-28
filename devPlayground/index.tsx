27c4f5368a4cee169e4d14b699c7915d6ebbe591
Boris Gingold <bogeee@bogitech.de>
2024-10-28 22:50:55 +0100

    - Build setup for publishing to browser (taken from restfuncs) and to also use it from the playground instantly without build

--- devPlayground/index.tsx
+++ devPlayground/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import {useWatchedState} from "react-deepwatch/develop";
+
 
 function App(props) {
+    useWatchedState({});
     return <div>hello</div>
 }
 
