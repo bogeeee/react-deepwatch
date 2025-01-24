8768ee235884420032564f69dd1a22499c1ae18f
Boris Gingold <bogeee@bogitech.de>
2025-01-24 11:12:55 +0100

    Revert: Accidentially deleted tests in commit 311ddc72

--- react-deepwatch/watchedGraph.test.ts
+++ react-deepwatch/watchedGraph.test.ts
@@ -25,6 +25,486 @@ function createSampleObjectGraph() {
 }
 
 
+describe('ProxiedGraph tests', () => {
+    test("Base implementation", () => {
+        const sampleGraph = createSampleObjectGraph();
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(sampleGraph);
+        expect(proxy !== sampleGraph).toBeTruthy();
+        expect(watchedGraph.getProxyFor(proxy) === proxy).toBeTruthy(); // Should return the proxy again
+        expect(proxy.appName).toBe("HelloApp");
+        expect(proxy.users === sampleGraph.users).toBe(false);
+        expect(proxy.users.length).toBe(2);
+    })
+
+    test("Arrays", () => {
+        const origArray = ["a", "b", "c"]
+        const proxy = new WatchedGraph().getProxyFor(origArray);
+        expect(proxy[0]).toBe("a");
+        expect(proxy.length).toBe(3);
+
+        const collected = [];
+        for(const i of proxy) {
+            collected.push(i);
+        }
+        expect(collected).toEqual(origArray);
+    })
+
+
+
+    test("Functions. 'this' should be the proxy", () => {
+        const origObj = {
+            thisIsOrigObj() {
+                return this === origObj;
+            }
+        };
+        const proxy = new WatchedGraph().getProxyFor(origObj);
+        expect(proxy.thisIsOrigObj()).toBeFalsy();
+    })
+
+    test("Property accessors. 'this' should be the proxy", () => {
+        const origObj = {
+            get thisIsOrigObj() {
+                return this === origObj;
+            },
+
+            set checkThisShouldNotBeOrigObj(value: string) {
+                if(this === origObj) {
+                    throw new Error("Assertion check failed");
+                }
+            }
+        };
+        const proxy = new WatchedGraph().getProxyFor(origObj);
+        expect(proxy.thisIsOrigObj).toBeFalsy();
+        proxy.checkThisShouldNotBeOrigObj = "dummy";
+    })
+
+    test("Property accessors. 'this' should be the proxy - for subclasses", () => {
+        let origObj: any;
+        class Base {
+            get thisIsOrigObj() {
+                return this === origObj;
+            }
+
+            set checkThisShouldNotBeOrigObj(value: string) {
+                if(this === origObj) {
+                    throw new Error("Assertion check failed");
+                }
+            }
+        }
+        class Sub extends Base {
+
+        }
+        origObj = new Sub();
+
+        const proxy = new WatchedGraph().getProxyFor(origObj);
+        expect(proxy.thisIsOrigObj).toBeFalsy();
+        proxy.checkThisShouldNotBeOrigObj = "dummy";
+    })
+
+    test("Property accessors: 'this' should be the topmost proxy when using 2 layers of proxies", () => {
+        const origObj = {
+            get thisIsProxy2() {
+                return this === proxy2;
+            },
+
+            set checkThisShouldBeProxy2(value: string) {
+                if(this !== proxy2) {
+                    throw new Error("Assertion check failed");
+                }
+            }
+        };
+        const proxy1 = new WatchedGraph().getProxyFor(origObj);
+        const proxy2 = new WatchedGraph().getProxyFor(proxy1);
+        expect(proxy2.thisIsProxy2).toBeTruthy();
+        proxy2.checkThisShouldBeProxy2 = "dummy";
+    })
+
+    test("Set a property that does not exist", () => {
+        const origObj = {} as any;
+        const proxy = new WatchedGraph().getProxyFor(origObj);
+        const subObj = {};
+        proxy.myNewProperty = subObj
+        expect(proxy.myNewProperty === subObj).toBeFalsy(); // Should be a proxy of it
+        expect(Object.keys(proxy)).toEqual(["myNewProperty"]);
+    })
+
+
+    test("instaceof", () => {
+        class MyClass {
+
+        }
+        const origObj = new MyClass();
+        const proxy = new WatchedGraph().getProxyFor(origObj);
+        expect(proxy instanceof MyClass).toBeTruthy();
+    });
+
+    test("delete property", () => {
+        const origObj = {
+            a: "b"
+        };
+        const proxy = new WatchedGraph().getProxyFor(origObj);
+        expect(Reflect.ownKeys(proxy)).toStrictEqual(["a"]);
+        //@ts-ignore
+        delete proxy.a;
+        expect(proxy.a).toBeUndefined();
+        expect(Reflect.ownKeys(proxy)).toStrictEqual([]);
+    });
+
+
+
+
+    test("Readonly props should not cause an error - fails - skipped", ()=> {
+        return; // skip, cause we wont fix this soon
+
+        const orig:{prop: object} = {} as any
+        Object.defineProperty(orig, "prop", {
+            value: {},
+            writable: false
+        })
+
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(orig);
+        expect(proxy.prop).toStrictEqual(orig.prop);
+    })
+
+});
+
+describe('ProxiedGraph and direct enhancement tests', () => {
+    for (const mode of [{
+        name: "ProxiedGraph", proxyOrEnhance<T extends object>(o: T) {
+            return new WatchedGraph().getProxyFor(o)
+        }
+    }, {
+        name: "Direct enhancement", proxyOrEnhance<T extends object>(o: T) {
+            enhanceWithWriteTracker(o);
+            return o;
+        }
+    }]) {
+
+        test(`${mode.name}: Object.keys`, () => {
+            const origObj = {a: "x", arr:["a","b"]};
+            const proxy = mode.proxyOrEnhance(origObj);
+            expect(Object.keys(proxy)).toEqual(["a", "arr"]);
+            expect(Object.keys(proxy.arr)).toEqual(Object.keys(origObj.arr));
+        })
+
+        test(`${mode.name}: Non modification`, () => {
+            const origObj = {a: "a"};
+            const proxy = mode.proxyOrEnhance(origObj);
+            proxy.a = "a"; // Should at least not trigger an error
+        })
+
+        test(`${mode.name}: Property accessors`, () => {
+            const origObj = new class {
+                get artificialProprty() {
+                    return "some";
+                }
+
+                _a = "";
+
+                get a() {
+                    return this._a;
+                }
+
+                set a(value: string) {
+                    this._a = value;
+                }
+
+                set setMe(value: string) {
+                    this._a = value;
+                }
+            }
+
+            const proxy = mode.proxyOrEnhance(origObj);
+
+            expect(proxy.a).toEqual("");
+
+            proxy.a = "x"
+            expect(proxy.a).toEqual("x");
+
+            proxy.setMe = "y"
+            expect(proxy.a).toEqual("y");
+
+            expect(proxy.artificialProprty).toEqual("some");
+
+        })
+
+        test(`${mode.name}: Readonly props from prototypes should not cause an error`, ()=> {
+            class A {
+                prop!: object
+            }
+            Object.defineProperty(A.prototype, "prop", {
+                value: {},
+                writable: false
+            })
+
+            const orig = new A();
+
+            const proxy = mode.proxyOrEnhance(orig);
+            expect(proxy.prop).toStrictEqual(orig.prop);
+        })
+
+        test(`${mode.name}: Class hierarchy should be intact`, ()=> {
+            let called: string = "";
+            class A {
+                myMethodOnlyA() {
+                    return "a"
+                }
+                get propWithGetterOnlyA() {
+                    return "a";
+                }
+                set setterOnlyA(value: string) {
+                    if(value !== "v") {
+                        throw new Error("invalid value")
+                    }
+                    called+="a";
+                }
+
+                myMethod() {
+                    return "a"
+                }
+
+                myMethodWithSuper() {
+                    return "a"
+                }
+                get propWithSuperGetter() {
+                    return "a";
+                }
+                set setterWithSuper(value: string) {
+                    if(value !== "v") {
+                        throw new Error("invalid value")
+                    }
+                    called+="a";
+                }
+
+            }
+
+            class B extends A {
+                myMethod() {
+                    return "b"
+                }
+
+                myMethodWithSuper() {
+                    return super.myMethodWithSuper() + "b";
+                }
+
+                get propWithGetter() {
+                    return "b";
+                }
+                get propWithSuperGetter() {
+                    return super.propWithSuperGetter + "b";
+                }
+                set setterWithSuper(value: string) {
+                    if(value !== "v") {
+                        throw new Error("invalid value")
+                    }
+                    super.setterWithSuper = value;
+                    called+="b";
+                }
+
+            }
+
+            const b = mode.proxyOrEnhance(new B());
+            expect(b.myMethod()).toEqual("b");
+            expect(b.myMethodOnlyA()).toEqual("a");
+            expect(b.myMethodWithSuper()).toEqual("ab");
+            expect(b.propWithGetter).toEqual("b");
+            expect(b.propWithGetterOnlyA).toEqual("a");
+            expect(b.propWithSuperGetter).toEqual("ab");
+            called="";b.setterOnlyA = "v";expect(called).toEqual("a");
+            called="";b.setterWithSuper = "v";expect(called).toEqual("ab");
+
+            expect(b instanceof B).toBeTruthy();
+            expect(b instanceof A).toBeTruthy();
+
+        });
+
+        test(`${mode.name}: Writes arrive`, ()=> {
+            const orig:any = {a: "x", counter: 0}
+            const proxy = mode.proxyOrEnhance(orig);
+            expect(proxy.a).toEqual("x");
+            proxy.b = "2"
+            expect(proxy.b).toEqual("2");
+            expect(orig.b).toEqual("2");
+            orig.c = "3"
+            expect(proxy.c).toEqual("3");
+
+            proxy.counter++;
+            proxy.counter++;
+            expect(proxy.counter).toEqual(2);
+        } )
+
+        /*
+        // Not possible with enhancement
+        test(`${mode.name}: Prototype should be the same`, () => {
+            const orig:any = {a: "x", counter: 0}
+            const protoOrig = Object.getPrototypeOf(orig);
+            const proxy = mode.proxyOrEnhance(orig);
+            expect(protoOrig === Object.getPrototypeOf(proxy)).toBeTruthy();
+
+        });
+        */
+        test(`${mode.name}: Constructor should be the same`, () => {
+            for(const obj of [{}, new Set, new Map, []]) {
+                const orig: object = obj
+                const constructorOrig = obj.constructor;
+                const proxy = mode.proxyOrEnhance(orig);
+                expect(constructorOrig === proxy.constructor).toBeTruthy();
+            }
+
+        });
+
+    }
+});
+
+describe('WatchedGraph tests', () => {
+    function readsEqual(reads: RecordedPropertyRead[], expected: { obj: object, key?: ObjKey, value?: unknown, values?: unknown[] }[]) {
+        function arraysAreShallowlyEqual(a?: unknown[], b?: unknown[]) {
+            if((a === undefined) && (b === undefined)) {
+                return true;
+            }
+            if(a === undefined || b === undefined) {
+                return false;
+            }
+            if(a.length !== b.length) {
+                return false;
+            }
+            for(let i = 0;i<a.length;i++) {
+                if(a[i] !== b[i]) { // TODO add option for object instance equality
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return arraysAreEqualsByPredicateFn(reads, expected, (propRead, exp) => {
+            return propRead.obj === exp.obj && propRead.key === exp.key && propRead.value === exp.value && arraysAreShallowlyEqual((propRead as unknown as RecordedArrayValuesRead).values, exp.values);
+        })
+    }
+
+    test("onAfterRead", () => {
+        const sampleGraph = createSampleObjectGraph();
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(sampleGraph);
+        let reads: RecordedPropertyRead[] = [];
+        watchedGraph.onAfterRead(r => reads.push(r as RecordedPropertyRead));
+
+        reads = [];
+        expect(proxy.appName).toBeDefined();
+        expect(readsEqual(reads, [{obj: sampleGraph, key: "appName", value: "HelloApp"}])).toBeTruthy();
+
+        reads = [];
+        expect(proxy.nullable).toBeNull();
+        expect(readsEqual(reads, [{obj: sampleGraph, key: "nullable", value: null}])).toBeTruthy();
+
+        reads = [];
+        expect(proxy.users[0]).toBeDefined();
+        expect(readsEqual(reads, [
+            {obj: sampleGraph, key: "users", value: sampleGraph.users},
+            {obj: sampleGraph.users, key: "0", value: sampleGraph.users[0]}
+        ])).toBeTruthy();
+    })
+
+    test("onAfterRead - iterate array", () => {
+        const sampleGraph = createSampleObjectGraph();
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(sampleGraph);
+        let reads: RecordedPropertyRead[] = [];
+        watchedGraph.onAfterRead(r => reads.push(r as RecordedPropertyRead));
+
+        // Iterate an array
+        reads = [];
+        proxy.users.forEach(user => expect(user).toBeDefined());
+        expect(readsEqual(reads, [
+            {obj: sampleGraph, key: "users", value: sampleGraph.users},
+            {obj: sampleGraph.users, values: sampleGraph.users},
+            {obj: sampleGraph.users, key: "0", value: sampleGraph.users[0]},
+            {obj: sampleGraph.users, key: "1", value: sampleGraph.users[1]},
+        ])).toBeTruthy();
+
+    });
+
+    test("onAfterRead - whitebox getters", () => {
+        const origObj = {
+            _prop: true,
+            get prop() {
+                return this._prop;
+            }
+        }
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(origObj);
+
+        // Install listener:
+        let reads: RecordedPropertyRead[] = [];
+        watchedGraph.onAfterRead(r => reads.push(r as RecordedPropertyRead));
+
+        expect(proxy.prop).toBeDefined();
+        expect(readsEqual(reads,[{obj: origObj, key: "_prop", value: true}])).toBeTruthy();
+    });
+
+    test("onAfterWrite", () => {
+        const sampleGraph = createSampleObjectGraph();
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(sampleGraph);
+
+        // Install listener:
+        let writes: unknown[] = [];
+        watchedGraph.onAfterWriteOnProperty(sampleGraph, "appName", (newValue) => writes.push(newValue));
+
+        proxy.appName = "xyz"; proxy.appName = "123";
+        expect(writes.length).toEqual(2)
+
+    });
+
+    test("onAfterWrite increase counter with ++", () => {
+        const sampleGraph = {counter: 0};
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(sampleGraph);
+
+        // Install listener:
+        let writes: unknown[] = [];
+        watchedGraph.onAfterWriteOnProperty(sampleGraph, "counter", (newValue) => writes.push(newValue));
+
+        proxy.counter++;
+        expect(writes.length).toEqual(1);
+
+    });
+
+    test("onAfterWrite expect to receive non-proxied objects or undefined", () => {
+        const origObject: {myProp?: object} = {
+            myProp: undefined,
+        };
+        const valueObj = {}
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(origObject);
+
+        // Install listener:
+        let writes: unknown[] = [];
+        watchedGraph.onAfterWriteOnProperty(origObject, "myProp", (newValue) => writes.push(newValue));
+
+        proxy.myProp = valueObj;
+        expect(writes[0] === undefined || writes[0] === valueObj).toBeTruthy()
+    });
+
+    it("should not fire onChange when value stays the same", ()=> {
+        // TODO
+    })
+
+    test("isArray should work on a proxy", () => {
+        const origObj: any[] = [];
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(origObj);
+        expect(Array.isArray(proxy)).toBeTruthy();
+        expect(_.isArray(proxy)).toBeTruthy();
+    })
+
+    test("Template", () => {
+        const sampleGraph = createSampleObjectGraph();
+        let watchedGraph = new WatchedGraph();
+        const proxy = watchedGraph.getProxyFor(sampleGraph);
+    });
+});
 
 
 describe('WatchedGraph record read and watch it', () => {
