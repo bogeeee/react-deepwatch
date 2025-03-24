4d148af5a6ebb23d56913f84e6921acb0b123562
Boris Gingold <bogeee@bogitech.de>
2025-03-24 20:36:42 +0100

    Improve examples. Better less-loading-code example

--- examples/less-loading-code/index.jsx
+++ examples/less-loading-code/index.jsx
@@ -2,20 +2,28 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import {watchedComponent, useWatchedState, watched, load, poll, isLoading, loadFailed, preserve, bind} from "react-deepwatch"
 
-
-async function myFetchFromServer() {
-    return (await(await fetch("example.json")).json()).name
+// async function that returns fruits after one second
+function simulateFetchFruitsFromServer(filter) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(["Apple", "Banana", "Cherry", "Date", "Fig", "Grape", "Kiwi", "Lemon", "Mango", "Orange", "Peach", "Pear", "Pineapple", "Pomegranate", "Raspberry", "Strawberry", "Tangerine", "Watermelon"].filter(f => !filter || f.indexOf(filter) >= 0))
+        }, 1000)
+    })
 }
 
+// Will reload the fruits and show a 🌀, if you type in the filter box. Will not reload when you change the show prices checkbox, because react-deepwatch sees, that it does not depend on it;)
 const MyComponent = watchedComponent(props => {
-
-    if(isLoading()) return <div>🌀</div>
+    const state = useWatchedState({
+        filter: "",
+        showPrices: false,
+    })
 
     return <div>
-        Here's something fetched from the Server: {  load( async () => await myFetchFromServer(), {/* LoadOptions (optional) */} )  }
+        Filter      <input type="text"     {...bind(state.filter    )} />
+        <div>Here are the fruits, fetched from the Server: {  load(() => simulateFetchFruitsFromServer(state.filter),{fallback:["loadinng list 🌀"]}).join(", ") }</div>
+        Show prices <input type="checkbox" {...bind(state.showPrices)} />
+        {state.showPrices?<div>Free today!</div>:null}
     </div>
-}, {/* WatchedComponentOptions (optional) */});
-
-<MyComponent/> // Use MyComponent
+});
 
 createRoot(document.getElementById('root')).render(<MyComponent/>);
