9f727b33ca8a8f9a977728c7a4155da86e26cd67
Boris Gingold <bogeee@bogitech.de>
2026-01-02 21:52:25 +0100

    Preserve: Support Sets and Maps -> update visitReplace util method (copied from restfuncs)

--- react-deepwatch/Util.ts
+++ react-deepwatch/Util.ts
@@ -197,6 +197,18 @@ function diagnosis_jsonPath(key: unknown) {
     return `.${key}`;
 }
 
+type VisitReplaceOptions = {
+    /**
+     * whether to pass on the context object. This hurts performance because the path is concatted every time, so use it only when needed. Setting this to "onError" re-executes the visitprelace with trackPath when an error was thrown
+     */
+    trackPath?: boolean | "onError",
+
+    /**
+     * Keep the order of sets and maps when replacing elements. Default: false
+     */
+    keepOrder?: boolean
+}
+
 /**
  * Usage:
  *  <pre><code>
@@ -207,9 +219,8 @@ function diagnosis_jsonPath(key: unknown) {
  *
  * @param value
  * @param visitor
- * @param trackPath whether to pass on the context object. This hurts performance because the path is concatted every time, so use it only when needed. Setting this to "onError" re-executes the visitprelace with the concetxt when an error was thrown
  */
-export function visitReplace<O>(value: O, visitor: (value: unknown, visitChilds: (value: unknown, context: VisitReplaceContext) => unknown, context: VisitReplaceContext) => unknown , trackPath: boolean | "onError" = false): O {
+export function visitReplace<O extends object>(value: O, visitor: (value: unknown, visitChilds: (value: unknown, context: VisitReplaceContext) => unknown, context: VisitReplaceContext) => unknown , options: VisitReplaceOptions= {}): O {
     const visisitedObjects = new Set<object>()
 
     function visitChilds(value: unknown, context: VisitReplaceContext) {
@@ -218,34 +229,99 @@ export function visitReplace<O>(value: O, visitor: (value: unknown, visitChilds:
         }
         else if(typeof value === "object") {
             const obj = value as object;
-            if(visisitedObjects.has(obj)) {
+            if (visisitedObjects.has(obj)) {
                 return value; // don't iterate again
             }
             visisitedObjects.add(obj);
 
-            for (let k in obj) {
-                const keyInParent = k as keyof object;
-                const childValue = obj[keyInParent];
-                let newValue = visitor(childValue, visitChilds, {...context, parentObject: value, key: keyInParent, diagnosis_path: (context.diagnosis_path !== undefined?`${context.diagnosis_path!}${diagnosis_jsonPath(keyInParent)}`:undefined)});
-                if(newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
-                    // @ts-ignore
-                    obj[keyInParent] = newValue;
+            if (value instanceof Set) {
+                let set = obj as Set<unknown>;
+                if (options.keepOrder) {
+                    throw new Error("Keep order not yet implemented");
+                }
+                for (const childValue of [...set.values()]) {
+                    let newValue = visitor(childValue, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: childValue,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? context.diagnosis_path! : undefined)
+                    });
+
+                    if (newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        // Replace:
+                        set.delete(childValue); set.add(newValue);
+                    }
+                }
+
+            }
+            else if (value instanceof Map) {
+                let map = obj as Map<unknown, unknown>;
+                if (options.keepOrder) {
+                    throw new Error("Keep order not yet implemented");
+                }
+
+                // Keys:
+                for (const key of [...map.keys()]) {
+                    let newKey = visitor(key, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: key,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? `${context.diagnosis_path!}${diagnosis_jsonPath(key)}` : undefined)
+                    });
+
+                    if (newKey !== key) { // Only if key really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        // Replace key:
+                        let value = map.get(key);
+                        map.delete(key);
+                        map.set(newKey, value);
+                    }
+                }
+
+                // Values:
+                for (const [key, childValue] of [...map.entries()]) {
+                    let newValue = visitor(childValue, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: key,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? `${context.diagnosis_path!}${diagnosis_jsonPath(key)}` : undefined)
+                    });
+
+                    if (newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        map.set(key, newValue);
+                    }
+                }
+
+            }
+            else {
+                for (let k in obj) {
+                    const keyInParent = k as keyof object;
+                    const childValue = obj[keyInParent];
+                    let newValue = visitor(childValue, visitChilds, {
+                        ...context,
+                        parentObject: value,
+                        key: keyInParent,
+                        diagnosis_path: (context.diagnosis_path !== undefined ? `${context.diagnosis_path!}${diagnosis_jsonPath(keyInParent)}` : undefined)
+                    });
+                    if (newValue !== childValue) { // Only if childValue really has changed. We don't want to interfer with setting a readonly property and trigger a proxy
+                        // @ts-ignore
+                        obj[keyInParent] = newValue;
+                    }
                 }
             }
         }
         return value;
     }
 
-    if(trackPath === "onError") {
+    if(options.trackPath === "onError") {
         try {
             return visitor(value,  visitChilds, {}) as O; // Fast try without context
         }
         catch (e) {
-            return visitReplace(value,  visitor, true); // Try again with context
+            return visitReplace(value,  visitor, {...options, trackPath: true}); // Try again with context
         }
     }
 
-    return visitor(value, visitChilds,{diagnosis_path: trackPath?"":undefined}) as O;
+    return visitor(value, visitChilds,{diagnosis_path: options.trackPath?"":undefined}) as O;
 }
 
 /**
